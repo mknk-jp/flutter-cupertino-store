d9faf3c5b3b2e96a54bb0ed5175466216fa1514e
mknk-jp <kanemasa1031@gmail.com>
2020-07-11 21:01:45 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:cupertino_store/model/app_state_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'app.dart';
+
+void main() => runApp(ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    ));
